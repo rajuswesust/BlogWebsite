1be360ebd791ee5dde74340db2aef23e3f46f035
raju singho <53114843+rajuswesust@users.noreply.github.com>
2022-12-25 17:54:54 +0600

    Update auth.route.js

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -2,7 +2,6 @@ const express = require('express');
 const  authtController = require('../controllers/auth.controller');
 const router = express.Router();
 
-router.get('/bal', authtController.dhon);
 router.post("/login", authtController.login);
 router.post("/logout", authtController.logout);
 router.post("/register", authtController.register);
